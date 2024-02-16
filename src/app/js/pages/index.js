ddfc73e8b85b3c7aba2b5c53b89fb04e9f04b0c7
Dendosha <thenocho22@gmail.com>
2024-02-16 18:53:39 +0500

    chore: rewrite some html markup and styles

--- src/app/js/pages/index.js
+++ src/app/js/pages/index.js
@@ -1,8 +1,15 @@
-import Slider from "../../../libs/a11y-slider/a11y-slider.js";
+import Slider from "../../../libs/a11y-slider/a11ySlider.js";
 
-const pageSliders = document.querySelectorAll('[data-slider]')
+const pageSliders = document.querySelectorAll('[data-slider="slider"]')
 pageSliders.forEach(slider => {
 	new Slider(slider, {
+		buttons: {
+			prevButton: slider.querySelector('[data-slider="prevButton"]'),
+			nextButton: slider.querySelector('[data-slider="nextButton"]'),
+		},
 
+		pagination: {
+			element: slider.querySelector('[data-slider="pagination"]'),
+		}
 	})
 })
